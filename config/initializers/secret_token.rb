9ce751b6b71270e63e97d1f5cefc427e38f7c9d0
Kristján Pétursson <kristjan@gmail.com>
2013-01-05 09:51:44 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threesquare::Application.config.secret_token = 'd72b094d31cc135eda933ae3c3a69f94e8110b9d652f99649d5dbc4a82cfa2b3de7ac3333c7ff0fc3be364fa936730b3ed426ffbb742395690cde0082b7147d3'
