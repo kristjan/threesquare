49f4d47970d1d4c2321f20bfa707f6f1ef4eb03e
Kristján Pétursson <kristjan@gmail.com>
2013-01-05 17:06:57 -0800

    Show the user last time's food on their checkin
    If the user has eaten here before, we'll tell them what they had and how they
    liked it.

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -7,9 +7,22 @@ class Checkin < ActiveRecord::Base
   def reply
     user.client.create_checkin_reply(
       foursquare_id,
-      text: "Something smells good! What did you order?",
+      text: reply_message,
       url: url
     )
+  end
+
+  def reply_message
+    if foods.empty?
+      "Something smells good! What did you order?"
+    else
+      last = foods.last
+      <<-EOS.squish
+        You've eaten #{foods.count} things here.
+        Last time, you thought the <strong>#{last.description}</strong>
+        was <strong>#{last.rating_as_emotion}</strong>.
+      EOS
+    end
   end
 
   def url
