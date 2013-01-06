ecbf76ba2f38ed5984e7c1a4867d629fabef40a4
Kristján Pétursson <kristjan@gmail.com>
2013-01-05 16:58:58 -0800

    Show user confirmation after submitting food
    Real basic, just let them know all is well.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,9 +7,16 @@ class FoodsController < ApplicationController
   def create
     @food = Food.new(params[:food])
     if @food.save
-      redirect_to params[:redirect_url] || root_path
+      redirect_to checkin_food_url(@food.checkin, @food,
+                    :redirect_url => params[:redirect_url]
+                  )
     else
       render :json => @food.errors
     end
+  end
+
+  def show
+    @food = Food.find(params[:id])
+    @redirect = params[:redirect_url] || root_path
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,22 @@
 class Food < ActiveRecord::Base
   attr_accessible :checkin_id, :description, :rating, :venue_id
 
-  has_one :checkin
+  belongs_to :checkin
 
   validates_presence_of :description
   validates_numericality_of :rating, :in => -1..1
+
+  EMOTIONS = {
+    -1 => :bad,
+     0 => :fine,
+     1 => :great
+  }
+
+  def rating_as_emotion
+    EMOTIONS[rating]
+  end
+
+  def smiley(yellow=false)
+    "smiley_#{rating_as_emotion}#{yellow ? '_yellow' : ''}.png"
+  end
 end
