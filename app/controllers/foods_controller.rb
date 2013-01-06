983e7818c1eb63ecb131fc89a9bbbb64b30461e2
Kristján Pétursson <kristjan@gmail.com>
2013-01-05 16:41:53 -0800

    Save Food objects
    Doesn't do anything smart for the user yet, but we store what we need.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,4 +3,13 @@ class FoodsController < ApplicationController
     @checkin = Checkin.find(params[:checkin_id])
     @food = @checkin.foods.new
   end
+
+  def create
+    @food = Food.new(params[:food])
+    if @food.save
+      render :text => "Got it!"
+    else
+      render :json => @food.errors
+    end
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,8 @@
 class Food < ActiveRecord::Base
   attr_accessible :checkin_id, :description, :rating, :venue_id
+
+  has_one :checkin
+
+  validates_presence_of :description
+  validates_numericality_of :rating, :in => -1..1
 end
